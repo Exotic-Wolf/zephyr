5be7f4e4a924d285edcc4456884006b744a219b4
Akshay Gopaul <wolf@Akshays-Mac-mini.local>
2026-05-20 02:44:12 +0400

    fix: hardcode Sentry DSN, add test endpoint

--- services/zephyr-api/src/instrument.ts
+++ services/zephyr-api/src/instrument.ts
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/nestjs';
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: 'https://2be82e22fe8b3d55251aa7282aa054da@o4511418834354176.ingest.us.sentry.io/4511418869219328',
   tracesSampleRate: 0.2,
 });
